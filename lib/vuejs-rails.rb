61465e3f5a98f970eb54fe12dcda977cd1a25939
YurySolovyov <biohazard707@gmail.com>
2017-04-04 16:28:30 +0300

    Use mattr_accessor and evaluate instead of require_asset

--- lib/vuejs-rails.rb
+++ lib/vuejs-rails.rb
@@ -1,6 +1,8 @@
 require "vuejs-rails/version"
 
 module Vue
+  mattr_accessor :minify
+
   module Rails
     require "vuejs-rails/engine" if defined?(::Rails) and Gem::Requirement.new('>= 3.1').satisfied_by?(Gem::Version.new ::Rails.version)
   end
